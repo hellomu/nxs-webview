0fae4decb3bb376ad96a1021853baae9fe92b9a4
hellomumu <mrs.liu@aliyun.com>
2020-08-26 10:30:57 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import FastClick from 'fastclick'
+import './plugins/vant.js'
+
+// 解决移动端点击延迟300ms的问题
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function () {
+      FastClick.attach(document.body);
+  }, false);
+}
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
